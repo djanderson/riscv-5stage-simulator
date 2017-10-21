7e25a03cd3fc4c88a27ac8855d7a17717f913090
djanderson <douglas.j.anderson@gmail.com>
2017-10-20 21:16:16 -0600

    Move instruction fields into sub-struct for easier initialization

--- src/instruction.rs
+++ src/instruction.rs
@@ -14,108 +14,85 @@ const RS2_SHIFT: u8 = 20;
 const RD_SHIFT: u8 = 7;
 
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub struct Instruction {
     instruction: u32,
-    pub rs1: Option<u32>,
-    pub rs2: Option<u32>,
-    pub rd: Option<u32>,
-    pub funct3: Option<u32>,
-    pub funct7: Option<u32>,
-    pub imm: Option<u32>,
-    pub shamt: Option<u32>,
-    pub opcode: Option<Rv32iOpcode>,
-    pub format: Option<Rv32InsnFormat>,
+    pub opcode: Opcode,
+    pub fields: Fields,
+    pub format: Format,
 }
 
 
 impl Instruction {
     pub fn new(instruction: u32) -> Instruction {
+        let opcode = int_to_opcode(instruction);
+        let format = opcode_to_format(opcode);
+        let fields = Fields::default();
         let mut insn = Instruction {
             instruction,
-            ..Default::default()
+            opcode,
+            format,
+            fields,
         };
         insn.parse();
         insn
     }
 
+    pub fn as_u32(&self) -> u32 {
+        self.instruction
+    }
+
     fn parse(&mut self) {
-        self.set_opcode();
-        self.set_format();
-        self.set_type_dependent_fields();
-    }
-
-    fn set_opcode(&mut self) {
-        self.opcode = Some(int_to_opcode(self.instruction));
-    }
-
-    fn set_format(&mut self) {
-        use self::Rv32iOpcode::*;
-        use self::Rv32InsnFormat::*;
-        self.format = Some(match self.opcode.unwrap() {
-            Lui => U,
-            AuiPc => U,
-            Jal => J,
-            Jalr => I,
-            Branch => B,
-            Load => I,
-            Store => S,
-            Op => R,
-            OpImm => I,
-        });
-    }
-
-    fn set_type_dependent_fields(&mut self) {
-        match self.format.unwrap() {
-            Rv32InsnFormat::R => self.parse_type_r(),
-            Rv32InsnFormat::I => self.parse_type_i(),
-            Rv32InsnFormat::S => self.parse_type_s(),
-            Rv32InsnFormat::B => self.parse_type_b(),
-            Rv32InsnFormat::U => self.parse_type_u(),
-            Rv32InsnFormat::J => self.parse_type_j(),
+        match self.format {
+            Format::R => self.parse_type_r(),
+            Format::I => self.parse_type_i(),
+            Format::S => self.parse_type_s(),
+            Format::B => self.parse_type_b(),
+            Format::U => self.parse_type_u(),
+            Format::J => self.parse_type_j(),
         };
     }
 
     fn parse_type_r(&mut self) {
         let insn = self.instruction;
-        self.funct3 = Some((insn & FUNCT3_MASK) >> FUNCT3_SHIFT);
-        self.funct7 = Some((insn & FUNCT7_MASK) >> FUNCT7_SHIFT);
-        self.rs1 = Some((insn & RS1_MASK) >> RS1_SHIFT);
-        self.rs2 = Some((insn & RS2_MASK) >> RS2_SHIFT);
-        self.rd = Some((insn & RD_MASK) >> RD_SHIFT);
+        self.fields.funct3 = Some((insn & FUNCT3_MASK) >> FUNCT3_SHIFT);
+        self.fields.funct7 = Some((insn & FUNCT7_MASK) >> FUNCT7_SHIFT);
+        self.fields.rs1 = Some((insn & RS1_MASK) >> RS1_SHIFT);
+        self.fields.rs2 = Some((insn & RS2_MASK) >> RS2_SHIFT);
+        self.fields.rd = Some((insn & RD_MASK) >> RD_SHIFT);
     }
 
     fn parse_type_i(&mut self) {
         let insn = self.instruction;
-        self.funct3 = Some((insn & FUNCT3_MASK) >> FUNCT3_SHIFT);
-        self.rs1 = Some((insn & RS1_MASK) >> RS1_SHIFT);
-        self.rd = Some((insn & RD_MASK) >> RD_SHIFT);
-        if self.funct3 == Some(0x1) || self.funct3 == Some(0x5) {
+        self.fields.funct3 = Some((insn & FUNCT3_MASK) >> FUNCT3_SHIFT);
+        self.fields.rs1 = Some((insn & RS1_MASK) >> RS1_SHIFT);
+        self.fields.rd = Some((insn & RD_MASK) >> RD_SHIFT);
+        if self.fields.funct3 == Some(0x1) || self.fields.funct3 == Some(0x5) {
             // Shift: insn[24:20] -> shamt
-            self.shamt = Some((insn & RS2_MASK) >> RS2_SHIFT)
+            self.fields.shamt = Some((insn & RS2_MASK) >> RS2_SHIFT)
         } else {
             // Arithmetic or logical: insn[31:20] -> imm[11:0]
-            self.imm = Some((insn & 0xfff00000) >> 20);
+            self.fields.imm = Some((insn & 0xfff00000) >> 20);
         }
     }
 
     fn parse_type_s(&mut self) {
         let insn = self.instruction;
-        self.funct3 = Some((insn & FUNCT3_MASK) >> FUNCT3_SHIFT);
-        self.rs1 = Some((insn & RS1_MASK) >> RS1_SHIFT);
-        self.rs2 = Some((insn & RS2_MASK) >> RS2_SHIFT);
+        self.fields.funct3 = Some((insn & FUNCT3_MASK) >> FUNCT3_SHIFT);
+        self.fields.rs1 = Some((insn & RS1_MASK) >> RS1_SHIFT);
+        self.fields.rs2 = Some((insn & RS2_MASK) >> RS2_SHIFT);
         // insn[31:25] -> imm[11:5]
         let imm_high = (insn & 0xfe000000) >> 20;
         // insn[11:7] -> imm[4:0]
         let imm_low = (insn & 0xF80) >> 7;
-        self.imm = Some(imm_high | imm_low);
+        self.fields.imm = Some(imm_high | imm_low);
     }
 
     fn parse_type_b(&mut self) {
         let insn = self.instruction;
-        self.funct3 = Some((insn & FUNCT3_MASK) >> FUNCT3_SHIFT);
-        self.rs1 = Some((insn & RS1_MASK) >> RS1_SHIFT);
-        self.rs2 = Some((insn & RS2_MASK) >> RS2_SHIFT);
+        self.fields.funct3 = Some((insn & FUNCT3_MASK) >> FUNCT3_SHIFT);
+        self.fields.rs1 = Some((insn & RS1_MASK) >> RS1_SHIFT);
+        self.fields.rs2 = Some((insn & RS2_MASK) >> RS2_SHIFT);
         // insn[7] -> imm[11]
         let imm_bit_11 = (insn & 0x80) << 4;
         // insn[31] -> imm[12]
@@ -124,19 +101,19 @@ impl Instruction {
         let imm_high = (insn & 0x7e000000) >> 20;
         // insn[11:8] -> imm[4:1]
         let imm_low = (insn & 0xf00) >> 7;
-        self.imm = Some(imm_bit_12 | imm_bit_11 | imm_high | imm_low);
+        self.fields.imm = Some(imm_bit_12 | imm_bit_11 | imm_high | imm_low);
     }
 
     fn parse_type_u(&mut self) {
         let insn = self.instruction;
-        self.rd = Some((insn & RD_MASK) >> RD_SHIFT);
+        self.fields.rd = Some((insn & RD_MASK) >> RD_SHIFT);
         // insn[31:12] -> imm[31:12]
-        self.imm = Some(insn & 0xfffff000);
+        self.fields.imm = Some(insn & 0xfffff000);
     }
 
     fn parse_type_j(&mut self) {
         let insn = self.instruction;
-        self.rd = Some((insn & RD_MASK) >> RD_SHIFT);
+        self.fields.rd = Some((insn & RD_MASK) >> RD_SHIFT);
         // insn[31] -> imm[20]
         let imm_bit_20 = (insn & 0x80000000) >> 11;
         // insn[30:21] -> imm[10:1]
@@ -145,31 +122,58 @@ impl Instruction {
         let imm_bit_11 = (insn & 0x100000) >> 9;
         // isns[19:12] -> imm[19:12]
         let imm_high = insn & 0xff000;
-        self.imm = Some(imm_bit_20 | imm_high | imm_bit_11 | imm_low);
+        self.fields.imm = Some(imm_bit_20 | imm_high | imm_bit_11 | imm_low);
     }
 }
 
 
-fn int_to_opcode(insn: u32) -> Rv32iOpcode {
-    use self::Rv32iOpcode::*;
+fn int_to_opcode(insn: u32) -> Opcode {
     let opcode = insn & OPCODE_MASK;
     match opcode {
-        0b01_101_11 => Lui,
-        0b00_101_11 => AuiPc,
-        0b11_011_11 => Jal,
-        0b11_001_11 => Jalr,
-        0b11_000_11 => Branch,
-        0b00_000_11 => Load,
-        0b01_000_11 => Store,
-        0b01_100_11 => Op,
-        0b00_100_11 => OpImm,
+        0b01_101_11 => Opcode::Lui,
+        0b00_101_11 => Opcode::AuiPc,
+        0b11_011_11 => Opcode::Jal,
+        0b11_001_11 => Opcode::Jalr,
+        0b11_000_11 => Opcode::Branch,
+        0b00_000_11 => Opcode::Load,
+        0b01_000_11 => Opcode::Store,
+        0b01_100_11 => Opcode::Op,
+        0b00_100_11 => Opcode::OpImm,
         _ => panic!("Unknown opcode {:#09b}", opcode),
     }
 }
 
 
+fn opcode_to_format(opcode: Opcode) -> Format {
+    match opcode {
+        Opcode::Lui => Format::U,
+        Opcode::AuiPc => Format::U,
+        Opcode::Jal => Format::J,
+        Opcode::Jalr => Format::I,
+        Opcode::Branch => Format::B,
+        Opcode::Load => Format::I,
+        Opcode::Store => Format::S,
+        Opcode::Op => Format::R,
+        Opcode::OpImm => Format::I,
+    }
+}
+
+
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+pub struct Fields {
+    pub rs1: Option<u32>,
+    pub rs2: Option<u32>,
+    pub rd: Option<u32>,
+    pub funct3: Option<u32>,
+    pub funct7: Option<u32>,
+    pub imm: Option<u32>,
+    pub shamt: Option<u32>,
+    pub opcode: Option<u32>,
+}
+
+
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub enum Rv32iOpcode {
+pub enum Opcode {
     Lui,
     AuiPc,
     Jal,
@@ -183,7 +187,7 @@ pub enum Rv32iOpcode {
 
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub enum Rv32InsnFormat {
+pub enum Format {
     R,
     I,
     S,
@@ -212,11 +216,11 @@ mod tests {
         // add x5, x6, x7
         let raw_insn = 0x7302b3;
         let parsed_insn = Instruction::new(raw_insn);
-        assert_eq!(parsed_insn.opcode.unwrap(), Rv32iOpcode::Op);
-        assert_eq!(parsed_insn.funct3.unwrap(), 0x0);
-        assert_eq!(parsed_insn.rd.unwrap(), 0x05);
-        assert_eq!(parsed_insn.rs1.unwrap(), 0x06);
-        assert_eq!(parsed_insn.rs2.unwrap(), 0x07);
+        assert_eq!(parsed_insn.opcode, Opcode::Op);
+        assert_eq!(parsed_insn.fields.funct3.unwrap(), 0x0);
+        assert_eq!(parsed_insn.fields.rd.unwrap(), 0x05);
+        assert_eq!(parsed_insn.fields.rs1.unwrap(), 0x06);
+        assert_eq!(parsed_insn.fields.rs2.unwrap(), 0x07);
     }
 
     #[test]
@@ -224,11 +228,11 @@ mod tests {
         // addi x5, x6, 20
         let raw_insn = 0x1430293;
         let parsed_insn = Instruction::new(raw_insn);
-        assert_eq!(parsed_insn.opcode.unwrap(), Rv32iOpcode::OpImm);
-        assert_eq!(parsed_insn.funct3.unwrap(), 0x0);
-        assert_eq!(parsed_insn.rd.unwrap(), 0x05);
-        assert_eq!(parsed_insn.rs1.unwrap(), 0x06);
-        assert_eq!(parsed_insn.imm.unwrap(), 20);
+        assert_eq!(parsed_insn.opcode, Opcode::OpImm);
+        assert_eq!(parsed_insn.fields.funct3.unwrap(), 0x0);
+        assert_eq!(parsed_insn.fields.rd.unwrap(), 0x05);
+        assert_eq!(parsed_insn.fields.rs1.unwrap(), 0x06);
+        assert_eq!(parsed_insn.fields.imm.unwrap(), 20);
     }
 
     #[test]
@@ -236,11 +240,11 @@ mod tests {
         // slli x5, x6, 3
         let raw_insn = 0x331293;
         let parsed_insn = Instruction::new(raw_insn);
-        assert_eq!(parsed_insn.opcode.unwrap(), Rv32iOpcode::OpImm);
-        assert_eq!(parsed_insn.funct3.unwrap(), 0x1);
-        assert_eq!(parsed_insn.rd.unwrap(), 0x05);
-        assert_eq!(parsed_insn.rs1.unwrap(), 0x06);
-        assert_eq!(parsed_insn.shamt.unwrap(), 3);
+        assert_eq!(parsed_insn.opcode, Opcode::OpImm);
+        assert_eq!(parsed_insn.fields.funct3.unwrap(), 0x1);
+        assert_eq!(parsed_insn.fields.rd.unwrap(), 0x05);
+        assert_eq!(parsed_insn.fields.rs1.unwrap(), 0x06);
+        assert_eq!(parsed_insn.fields.shamt.unwrap(), 3);
     }
 
     #[test]
@@ -248,11 +252,11 @@ mod tests {
         // sw x5, 40(x6)
         let raw_insn = 0x2532423;
         let parsed_insn = Instruction::new(raw_insn);
-        assert_eq!(parsed_insn.opcode.unwrap(), Rv32iOpcode::Store);
-        assert_eq!(parsed_insn.funct3.unwrap(), 0x2);
-        assert_eq!(parsed_insn.rs1.unwrap(), 0x06);
-        assert_eq!(parsed_insn.rs2.unwrap(), 0x05);
-        assert_eq!(parsed_insn.imm.unwrap(), 40);
+        assert_eq!(parsed_insn.opcode, Opcode::Store);
+        assert_eq!(parsed_insn.fields.funct3.unwrap(), 0x2);
+        assert_eq!(parsed_insn.fields.rs1.unwrap(), 0x06);
+        assert_eq!(parsed_insn.fields.rs2.unwrap(), 0x05);
+        assert_eq!(parsed_insn.fields.imm.unwrap(), 40);
     }
 
     #[test]
@@ -260,11 +264,11 @@ mod tests {
         // beq x5, x6, 100
         let raw_insn = 0x6628263;
         let parsed_insn = Instruction::new(raw_insn);
-        assert_eq!(parsed_insn.opcode.unwrap(), Rv32iOpcode::Branch);
-        assert_eq!(parsed_insn.funct3.unwrap(), 0x0);
-        assert_eq!(parsed_insn.rs1.unwrap(), 0x05);
-        assert_eq!(parsed_insn.rs2.unwrap(), 0x06);
-        assert_eq!(parsed_insn.imm.unwrap(), 100);
+        assert_eq!(parsed_insn.opcode, Opcode::Branch);
+        assert_eq!(parsed_insn.fields.funct3.unwrap(), 0x0);
+        assert_eq!(parsed_insn.fields.rs1.unwrap(), 0x05);
+        assert_eq!(parsed_insn.fields.rs2.unwrap(), 0x06);
+        assert_eq!(parsed_insn.fields.imm.unwrap(), 100);
     }
 
     #[test]
@@ -272,9 +276,9 @@ mod tests {
         // lui x5, 0x12345
         let raw_insn = 0x123452b7;
         let parsed_insn = Instruction::new(raw_insn);
-        assert_eq!(parsed_insn.opcode.unwrap(), Rv32iOpcode::Lui);
-        assert_eq!(parsed_insn.rd.unwrap(), 0x05);
-        assert_eq!(parsed_insn.imm.unwrap(), 0x12345000);
+        assert_eq!(parsed_insn.opcode, Opcode::Lui);
+        assert_eq!(parsed_insn.fields.rd.unwrap(), 0x05);
+        assert_eq!(parsed_insn.fields.imm.unwrap(), 0x12345000);
     }
 
     #[test]
@@ -282,9 +286,9 @@ mod tests {
         // jal x1, 100
         let raw_insn = 0x64000ef;
         let parsed_insn = Instruction::new(raw_insn);
-        assert_eq!(parsed_insn.opcode.unwrap(), Rv32iOpcode::Jal);
-        assert_eq!(parsed_insn.rd.unwrap(), 0x01);
-        assert_eq!(parsed_insn.imm.unwrap(), 100);
+        assert_eq!(parsed_insn.opcode, Opcode::Jal);
+        assert_eq!(parsed_insn.fields.rd.unwrap(), 0x01);
+        assert_eq!(parsed_insn.fields.imm.unwrap(), 100);
 
     }
 }
