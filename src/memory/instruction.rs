6fd6207ce8595e90ca332e1e93f9e148bcd8efc1
Douglas Anderson <douglas.j.anderson@gmail.com>
2017-11-16 20:46:34 -0700

    Document the instruction memory

--- src/memory/instruction.rs
+++ src/memory/instruction.rs
@@ -1,3 +1,7 @@
+//! Read-only instruction memory.
+//!
+//! Provides a loader for disassembler output.
+
 use instruction;
 
 use regex::{Captures, Regex};
@@ -5,47 +9,60 @@ use regex::{Captures, Regex};
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::num::ParseIntError;
 
 
+/// Regex to parse disassembler output
 const RE: &str = r"(?x)
     ^[[:blank:]]+
-    (?P<addr>[[:xdigit:]]+)
-    :
+    (?P<addr>[[:xdigit:]]+):    # Captures ... (addr): xx xx xx xx ...
     [[:blank:]]+
-    (?P<byte1>[[:xdigit:]]{2})
+    (?P<byte1>[[:xdigit:]]{2})  # Captures ... addr: (xx) xx xx xx ...
     [[:blank:]]
-    (?P<byte2>[[:xdigit:]]{2})
+    (?P<byte2>[[:xdigit:]]{2})  # Captures ... addr: xx (xx) xx xx ...
     [[:blank:]]
-    (?P<byte3>[[:xdigit:]]{2})
+    (?P<byte3>[[:xdigit:]]{2})  # Captures ... addr: xx xx (xx) xx ...
     [[:blank:]]
-    (?P<byte4>[[:xdigit:]]{2})
+    (?P<byte4>[[:xdigit:]]{2})  # Captures ... addr: xx xx xx (xx) ...
     .*$
 ";
 
 
+/// A read-only instruction memory.
 pub struct InstructionMemory {
     pub mem: Vec<u32>,
 }
 
 
 impl InstructionMemory {
-    pub fn new(f: &File) -> InstructionMemory {
-        let file = BufReader::new(f);
+    /// Constructs a new `InstructionMemory`.
+    ///
+    /// `disassembly` must be an open text file containing lines of the form:
+    ///
+    /// ```text
+    ///      16c:	00 15 05 13    addi x10 , x10 , 1
+    /// ```
+    ///
+    /// The first such matching line must have address 0.
+    /// Non-matching lines are ignored.
+    ///
+    pub fn new(disassembly: &File) -> InstructionMemory {
+        let file = BufReader::new(disassembly);
         let mut mem = Vec::new();
         let regex = Regex::new(RE).unwrap();
 
+        // Load each line of disassembly into memory
         for line in file.lines() {
-            let l = match line {
-                Ok(v) => v,
-                Err(e) => panic!("Couldn't read line: {:?}", e),
-            };
+            let l = line.expect("failed to read line");
             match regex.captures(&l) {
                 Some(caps) => {
-                    let addr = extract_addr(&caps);
-                    let insn = extract_insn(&caps);
+                    let addr = extract_addr(&caps).unwrap();
+                    let insn = extract_insn(&caps).unwrap();
                     mem.push(insn);
+                    // Test that addr matches the actual location in memory
                     assert_eq!(addr, ((mem.len() - 1) * 4) as u32);
                 }
+                // Ignore lines that don't match the regex
                 None => {}
             }
         }
@@ -55,6 +72,10 @@ impl InstructionMemory {
         InstructionMemory { mem }
     }
 
+    /// Reads an instruction from `InstructionMemory`.
+    ///
+    /// The requested address is right-shifted by 2 to ensure word alignment.
+    ///
     pub fn read(&self, addr: usize) -> u32 {
         let word_addr = addr >> 2;
 
@@ -67,23 +88,21 @@ impl InstructionMemory {
 }
 
 
-fn extract_addr(caps: &Captures) -> u32 {
+/// Extracts regex captures related to the memory address and converts to u32.
+fn extract_addr(caps: &Captures) -> Result<u32, ParseIntError> {
     let s = caps.name("addr").unwrap().as_str();
-    let addr = u32::from_str_radix(&s, 16).unwrap();
-
-    addr
+    u32::from_str_radix(&s, 16)
 }
 
 
-fn extract_insn(caps: &Captures) -> u32 {
+/// Extracts regex captures related to the instruction and converts to u32.
+fn extract_insn(caps: &Captures) -> Result<u32, ParseIntError> {
     let s: String = caps.name("byte1").unwrap().as_str().to_owned() +
         &caps.name("byte2").unwrap().as_str().to_owned() +
         &caps.name("byte3").unwrap().as_str().to_owned() +
         &caps.name("byte4").unwrap().as_str().to_owned();
 
-    let insn = u32::from_str_radix(&s, 16).unwrap();
-
-    insn
+    u32::from_str_radix(&s, 16)
 }
 
 
@@ -111,7 +130,7 @@ mod tests {
         let haystack: &str = "     16c:	00 15 05 13    addi x10 , x10 , 1";
         let needle = Regex::new(RE).unwrap();
         let caps = needle.captures(haystack).unwrap();
-        let addr = extract_addr(&caps);
+        let addr = extract_addr(&caps).unwrap();
         assert_eq!(addr, 0x16c);
     }
 
@@ -120,7 +139,7 @@ mod tests {
         let haystack: &str = "     16c:	00 15 05 13    addi x10 , x10 , 1";
         let needle = Regex::new(RE).unwrap();
         let caps = needle.captures(haystack).unwrap();
-        let insn = extract_insn(&caps);
+        let insn = extract_insn(&caps).unwrap();
         assert_eq!(insn, 0x00_15_05_13);
     }
 
