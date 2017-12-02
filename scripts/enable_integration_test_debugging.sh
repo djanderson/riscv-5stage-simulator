83f5ff7c40db94b1b9cb2fba62e416921ddc59e7
Douglas Anderson <douglas.j.anderson@gmail.com>
2017-12-01 23:54:19 -0700

    Add script to copy integration test resources into target directory for debugging

---
+++ scripts/enable_integration_test_debugging.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Copy resources files into the target directory so that integration tests can
+# be run in the debugger.
+#
+# You must first create the build directory with `cargo run` or `cargo test`.
+
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# Ensure target directory exists
+mkdir -p ${REPO_ROOT}/target/debug/tests
+
+# Remove existing/outdated files
+rm -f ${REPO_ROOT}/target/debug/tests/*
+
+# Copy current contents of ./tests into target directory
+cp -r ${REPO_ROOT}/tests/*.txt ${REPO_ROOT}/target/debug/tests/
+
+# Echo results
+FILES=${REPO_ROOT}/target/debug/tests/*.txt
+echo "Created the following files:"
+for f in $FILES; do
+    echo "$f"
+done
