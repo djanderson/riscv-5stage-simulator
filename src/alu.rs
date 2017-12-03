10ff0313cc0cc2f16c0428586c6d9a3bd33ae176
Douglas Anderson <douglas.j.anderson@gmail.com>
2017-12-03 11:16:02 -0700

    Use overflowing add, sub, and shifts in ALU simulator.
    Rust in debug mode panics at overflows, which is very awesome. But we're
    emulating an ALU, so I need to choose a hardware behavior and emulate that. I
    used integer's `overflowing_{add,bug,shl,shr}` methods, which does the standard
    2's complement overflow behavior and also returns a boolean indicating if an
    overlow happened or not.
    
    I found the following article very helpful: http://huonw.github.io/blog/2016/04/myths-and-legends-about-integer-overflow-in-rust/

--- src/alu.rs
+++ src/alu.rs
@@ -8,26 +8,43 @@ use instruction::Instruction;
 pub fn alu(insn: &Instruction, src1: i32, src2: i32) -> i32 {
     use self::AluOp::*;
 
-    let value = match insn.semantics.alu_op {
-        Add => src1 + src2,
-        Sub => src1 - src2,
-        And => src1 & src2,
-        Or => src1 | src2,
-        Xor => src1 ^ src2,
-        BranchOnEqual => !(src1 == src2) as i32,
-        BranchOnNotEqual => !(src1 != src2) as i32,
-        BranchOnLessThan => !(src1 < src2) as i32,
-        BranchOnLessThanUnsigned => !((src1 as u32) < (src2 as u32)) as i32,
-        BranchOnGreaterOrEqual => !(src1 >= src2) as i32,
-        BranchOnGreaterOrEqualUnsigned => {
-            !((src1 as u32) >= (src2 as u32)) as i32
-        }
-        ShiftLeft => src1 << src2,
-        ShiftRightLogical => ((src1 as u32) >> src2) as i32,
-        ShiftRightArithmetic => src1 >> src2,
-        SetOnLessThan => (src1 < src2) as i32,
-        SetOnLessThanUnsigned => ((src1 as u32) < (src2 as u32)) as i32,
+    let (value, overflow) = match insn.semantics.alu_op {
+        Add => src1.overflowing_add(src2),
+        Sub => src1.overflowing_sub(src2),
+        And => (src1 & src2, false),
+        Or => (src1 | src2, false),
+        Xor => (src1 ^ src2, false),
+        BranchOnEqual => (!(src1 == src2) as i32, false),
+        BranchOnNotEqual => (!(src1 != src2) as i32, false),
+        BranchOnLessThan => (!(src1 < src2) as i32, false),
+        BranchOnLessThanUnsigned => (
+            !((src1 as u32) < (src2 as u32)) as i32,
+            false,
+        ),
+        BranchOnGreaterOrEqual => (!(src1 >= src2) as i32, false),
+        BranchOnGreaterOrEqualUnsigned => (
+            !((src1 as u32) >= (src2 as u32)) as
+                i32,
+            false,
+        ),
+        ShiftLeft => (src1 << src2, false),
+        ShiftRightLogical => (((src1 as u32) >> src2) as i32, false),
+        ShiftRightArithmetic => src1.overflowing_shr(src2 as u32),
+        SetOnLessThan => ((src1 < src2) as i32, false),
+        SetOnLessThanUnsigned => (
+            ((src1 as u32) < (src2 as u32)) as i32,
+            false,
+        ),
     };
+
+    if overflow {
+        println!(
+            "Detected overflow {} {:?} {}",
+            src1,
+            insn.semantics.alu_op,
+            src2
+        );
+    }
 
     value
 }
